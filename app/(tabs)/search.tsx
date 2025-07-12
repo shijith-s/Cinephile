b614d050742ce207981eebc940aef30d92154feb
shijith_s <shijithsanthosh@gmail.com>
2025-07-13 03:17:03 +0530

    Implement search section

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,93 @@
-import { Text, View } from "react-native";
-import React from "react";
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import SearchedMoviesEmpty from "@/components/SearchedMoviesEmpty";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { debounce } from "@/utils";
+import React, { useEffect, useRef, useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
 
-const search = () => {
+const SearchPage = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const {
+    data: movies,
+    loading,
+    error,
+    refetch,
+    reset,
+  } = useFetch(fetchMovies, false);
+
+  const loadMovies = async (searchQuery: string = "") => {
+    if (searchQuery?.trim()) {
+      await refetch(searchQuery);
+    } else {
+      reset();
+    }
+  };
+
+  const debouncedLoadMovies = useRef(debounce(loadMovies)).current;
+
+  useEffect(() => {
+    debouncedLoadMovies(searchQuery);
+  }, [searchQuery]);
+
+  const handleSearch = (searchText: string) => {
+    setSearchQuery(searchText);
+  };
+
   return (
-    <View className="flex-1 items-center justify-center text-dark-100">
-      <Text className="text-2xl font-bold">search</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={3}
+        className="px-5"
+        columnWrapperStyle={{
+          justifyContent: "space-between",
+          marginBottom: 30,
+        }}
+        contentContainerStyle={{
+          paddingBottom: 100,
+          gap: 3,
+        }}
+        ListEmptyComponent={
+          <SearchedMoviesEmpty
+            searchTerm={searchQuery}
+            loading={loading}
+            error={error}
+          />
+        }
+        ListHeaderComponent={
+          <>
+            <View className="w-full flex-row items-center justify-center mt-20">
+              <Image source={icons.logo} className="w-12 h-10" />
+            </View>
+            <View className="my-5">
+              <SearchBar
+                value={searchQuery}
+                placeholder="Search movies..."
+                onChangeText={handleSearch}
+              />
+            </View>
+            {searchQuery && movies.length > 0 && (
+              <Text className="text-white text-left text-lg mb-6 mx-5">
+                Search results for{" "}
+                <Text className="text-light-100 font-semibold italic">
+                  {searchQuery}
+                </Text>
+              </Text>
+            )}
+          </>
+        }
+      />
     </View>
   );
 };
 
-export default search;
+export default SearchPage;
---
+++ components/SearchedMoviesEmpty.tsx
@@ -0,0 +1,38 @@
+import { ActivityIndicator, Text, View } from "react-native";
+import React from "react";
+import theme from "@/constants/theme";
+
+interface Props {
+  searchTerm?: string;
+  loading?: boolean;
+  error?: Error | null;
+}
+
+const SearchedMoviesEmpty = ({ searchTerm, loading, error }: Props) => {
+  const isEmpty = !loading && !error;
+  const isSearchTermPresent = searchTerm?.trim();
+  return (
+    <View className="flex-1 items-center justify-center">
+      {loading && (
+        <ActivityIndicator
+          size="large"
+          color={theme.colors.activityIndicator}
+        />
+      )}
+      {error && (
+        <Text className="text-white text-center">Error: {error?.message}</Text>
+      )}
+      {isEmpty && (
+        <View className="mt-10 px-5 flex-1 h-full items-center justify-center">
+          <Text className="text-gray-500 text-center">
+            {isSearchTermPresent
+              ? `No Movies Found for "${searchTerm}"`
+              : "Search for a movie"}
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default SearchedMoviesEmpty;
